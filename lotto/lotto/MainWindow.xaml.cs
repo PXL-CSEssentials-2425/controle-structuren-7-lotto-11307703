f3b3b1ed0de4086a3e03ded5b45e9a747d84da1b
11307703 <11307703@student.pxl.be>
2024-10-11 13:23:16 +0200

    Commit finaal

---
+++ lotto/lotto/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lotto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("De 6 Lotto getallen zijn: ");
+            sb.AppendLine("");
+            Random numberGen = new Random();
+
+            for (int i = 1; i < 7; i++)
+            {
+                int randomNr = numberGen.Next(0, 46);
+                string result = $"Getal {i} is {randomNr}";
+                sb.AppendLine(result);
+
+            }
+            resultTextbox.Text = sb.ToString();
+        }
+    }
+}
